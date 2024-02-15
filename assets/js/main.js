80f6e72f12a2ff83ffde85c227c82c76e632dc7d
shpetimsedolli <shpeti101@gmail.com>
2024-02-15 17:57:17 +0100

    animated h1 with javascript animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 const toggleButton = document.querySelector(".toggle-button");
 const navLinks = document.querySelector(".links");
 
-function scrollToBottom() {
-  window.scroll(0, document.body.scrollHeight);
-}
-
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
@@ -24,3 +20,13 @@ var loader = document.getElementById("preloader");
 var timeout = setTimeout(function () {
   loader.style.display = "none";
 }, 1000);
+
+function scrollToBottom() {
+  window.scroll(0, document.body.scrollHeight);
+}
+var typed = new Typed(".auto-type", {
+  strings: ["CHAT GPT -3", "NEW AI", "DESIGNER"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
